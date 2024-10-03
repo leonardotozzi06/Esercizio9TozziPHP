4b1852cb03d5a705a76dfed783965fbd1829ef04
leonardotozzi06 <tozzileonardo18@gmail.com>
2024-10-03 09:15:11 +0300

    Esercizio9TozziPHP

---
+++ EsercizioTozzi9.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+
+    $nome = "Patrick Cutrone";
+
+    $r = rand(0,255);
+
+    $g = rand(0,255);
+
+    $b = rand(0,255);
+
+
+    echo "<h1 style='color: rgb($r, $g, $b)'>$nome</h1>";
+
+    ?>
+
+</body>
+</html>
